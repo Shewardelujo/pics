9c1ac6a324e1df9bdea1839068052599eda38685
Shewardelujo <adesewa.adefalujo@gmail.com>
2020-10-15 14:19:50 +0100

    Little punctuation issue

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID WCFK303MFLmArf4K4exS0Rqsf4MSElrUWYURV6dx99o'
+    }
+});
